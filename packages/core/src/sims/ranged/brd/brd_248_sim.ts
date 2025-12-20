b0a8aed68ead8f96d0ac80656a64e25373883cdd
Joao Lippi <joao.p.lippi@gmail.com>
2025-12-20 17:16:24 -0300

    Values update 2.50

--- packages/core/src/sims/ranged/brd/brd_248_sim.ts
+++ packages/core/src/sims/ranged/brd/brd_248_sim.ts
@@ -1,5 +1,5 @@
 import {CharacterGearSet} from "../../../gear";
-import {SimResult, SimSettings, SimSpec, Simulation} from "../../sim_types";
+import {SimResult, SimSettings, SimSpec, Simulation, AutoAttack, GcdAbility, OgcdAbility} from "../../sim_types";
 import {EmptyObject} from "@xivgear/util/util_types";
 
 export const brd248SimSpec: SimSpec<Brd248Sim, SimSettings> = {
@@ -19,6 +19,355 @@ export const brd248SimSpec: SimSpec<Brd248Sim, SimSettings> = {
 
 export interface Brd248SimResult extends SimResult {
 }
+
+// BRD Skills List
+const heavyShot: GcdAbility = {
+    name: 'Heavy Shot',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 160,
+    gcd: 2.5,
+    id: 97,
+} as const satisfies GcdAbility;
+
+const venomousBite: GcdAbility = {
+    name: 'Venomous Bite',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 15,
+    gcd: 2.5,
+    id: 100,
+} as const satisfies GcdAbility;
+
+const quickNock: GcdAbility = {
+    name: 'Quick Nock',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 110,
+    gcd: 2.5,
+    id: 106,
+} as const satisfies GcdAbility;
+
+const bloodletter: OgcdAbility = {
+    name: 'Bloodletter',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: 130,
+    cooldown: {
+        time: 15,
+    },
+    id: 110,
+} as const satisfies OgcdAbility;
+
+const straightShot: GcdAbility = {
+    name: 'Straight Shot',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 200,
+    gcd: 2.5,
+    id: 98,
+} as const satisfies GcdAbility;
+
+const barrage: OgcdAbility = {
+    name: 'Barrage',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 120,
+    },
+    id: 107,
+} as const satisfies OgcdAbility;
+
+const windbite: GcdAbility = {
+    name: 'Windbite',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 20,
+    gcd: 2.5,
+    id: 113,
+} as const satisfies GcdAbility;
+
+const ragingStrikes: OgcdAbility = {
+    name: 'Raging Strikes',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 120,
+    },
+    id: 101,
+} as const satisfies OgcdAbility;
+
+const repellingShot: OgcdAbility = {
+    name: 'Repelling Shot',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 30,
+    },
+    id: 112,
+} as const satisfies OgcdAbility;
+
+const wideVolley: GcdAbility = {
+    name: 'Wide Volley',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 140,
+    gcd: 2.5,
+    id: 111,
+} as const satisfies GcdAbility;
+
+const magesBallad: OgcdAbility = {
+    name: "Mage's Ballad",
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 120,
+    },
+    id: 114,
+} as const satisfies OgcdAbility;
+
+const armysPaeon: OgcdAbility = {
+    name: "Army's Paeon",
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 120,
+    },
+    id: 116,
+} as const satisfies OgcdAbility;
+
+const rainOfDeath: OgcdAbility = {
+    name: 'Rain of Death',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: 100,
+    cooldown: {
+        time: 15,
+    },
+    id: 117,
+} as const satisfies OgcdAbility;
+
+const battleVoice: OgcdAbility = {
+    name: 'Battle Voice',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 120,
+    },
+    id: 118,
+} as const satisfies OgcdAbility;
+
+const empyrealArrow: OgcdAbility = {
+    name: 'Empyreal Arrow',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: 260,
+    cooldown: {
+        time: 15,
+    },
+    id: 3558,
+} as const satisfies OgcdAbility;
+
+const theWanderersMinuet: OgcdAbility = {
+    name: "The Wanderer's Minuet",
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 120,
+    },
+    id: 3559,
+} as const satisfies OgcdAbility;
+
+const ironJaws: GcdAbility = {
+    name: 'Iron Jaws',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 100,
+    gcd: 2.5,
+    id: 3560,
+} as const satisfies GcdAbility;
+
+const theWardensPaean: OgcdAbility = {
+    name: "The Warden's Paean",
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 45,
+    },
+    id: 3561,
+} as const satisfies OgcdAbility;
+
+const sidewinder: OgcdAbility = {
+    name: 'Sidewinder',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: 400,
+    cooldown: {
+        time: 60,
+    },
+    id: 3562,
+} as const satisfies OgcdAbility;
+
+const pitchPerfect: OgcdAbility = {
+    name: 'Pitch Perfect',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: 100, // Base potency, scales with stacks
+    cooldown: {
+        time: 1,
+    },
+    id: 7404,
+} as const satisfies OgcdAbility;
+
+const troubadour: OgcdAbility = {
+    name: 'Troubadour',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 90,
+    },
+    id: 7405,
+} as const satisfies OgcdAbility;
+
+const causticBite: GcdAbility = {
+    name: 'Caustic Bite',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 20,
+    gcd: 2.5,
+    id: 7406,
+} as const satisfies GcdAbility;
+
+const stormbite: GcdAbility = {
+    name: 'Stormbite',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 25,
+    gcd: 2.5,
+    id: 7407,
+} as const satisfies GcdAbility;
+
+const naturesMinne: OgcdAbility = {
+    name: "Nature's Minne",
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 120,
+    },
+    id: 7408,
+} as const satisfies OgcdAbility;
+
+const refulgentArrow: GcdAbility = {
+    name: 'Refulgent Arrow',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 280,
+    gcd: 2.5,
+    id: 7409,
+} as const satisfies GcdAbility;
+
+const shadowbite: GcdAbility = {
+    name: 'Shadowbite',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 170,
+    gcd: 2.5,
+    id: 16494,
+} as const satisfies GcdAbility;
+
+const burstShot: GcdAbility = {
+    name: 'Burst Shot',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 220,
+    gcd: 2.5,
+    id: 16495,
+} as const satisfies GcdAbility;
+
+const apexArrow: GcdAbility = {
+    name: 'Apex Arrow',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 600,
+    gcd: 2.5,
+    id: 16496,
+} as const satisfies GcdAbility;
+
+const ladonsbite: GcdAbility = {
+    name: 'Ladonsbite',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 130,
+    gcd: 2.5,
+    id: 25783,
+} as const satisfies GcdAbility;
+
+const blastArrow: GcdAbility = {
+    name: 'Blast Arrow',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 600,
+    gcd: 2.5,
+    id: 25784,
+} as const satisfies GcdAbility;
+
+const radiantFinale: OgcdAbility = {
+    name: 'Radiant Finale',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: null,
+    cooldown: {
+        time: 110,
+    },
+    id: 25785,
+} as const satisfies OgcdAbility;
+
+const heartbreakShot: OgcdAbility = {
+    name: 'Heartbreak Shot',
+    type: 'ogcd',
+    attackType: 'Ability',
+    potency: 180,
+    cooldown: {
+        time: 15,
+    },
+    id: 36975,
+} as const satisfies OgcdAbility;
+
+const resonantArrow: GcdAbility = {
+    name: 'Resonant Arrow',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 600,
+    gcd: 2.5,
+    id: 36976,
+} as const satisfies GcdAbility;
+
+const radiantEncore: GcdAbility = {
+    name: 'Radiant Encore',
+    type: 'gcd',
+    attackType: 'Weaponskill',
+    potency: 500, // Base potency, scales with stacks
+    gcd: 2.5,
+    id: 36977,
+} as const satisfies GcdAbility;
+
+const auto: AutoAttack = {
+    name: 'Auto Attack',
+    type: 'autoattack',
+    potency: 90,
+    attackType: 'Auto-attack',
+    id: 7,
+} as const satisfies AutoAttack;
 
 export class Brd248Sim implements Simulation<Brd248SimResult, SimSettings, EmptyObject> {
     exportSettings() {
